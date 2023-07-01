1d06c80e0b3366818f73b7463bab23dc934ea9eb
siddhartha.tyagi@wionews.com <siddhartha.tyagi@wionews.com>
2023-07-01 19:12:46 +0530

    how to build go lang

---
+++ 06buildcommand/go.mod
@@ -0,0 +1,3 @@
+module buildcommand
+
+go 1.19
---
+++ 06buildcommand/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Go Inside Terminal write go env")
+
+	fmt.Println("Find goos value ex: windows/linux and darwin for mac")
+    
+	fmt.Println("It will Create exe file for windows/linus/mac")
+
+	fmt.Println("On Terminal Type Build Command For Windows: GOOS='windows' go build")
+
+	fmt.Println("GOOS ='linux' go build")
+
+	fmt.Println("GOOS='darwin' go bui;")
+
+}
